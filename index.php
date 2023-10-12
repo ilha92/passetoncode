3999e72246997137b0b3b1baa1577c79adb6bd4c
Ilias Hamel <iliashamel041@gmail.com>
2023-10-13 01:10:15 +0200

    index.php corrige beug

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="style/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/sFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="images/logo.png">
     <style>
         body {
             background-image: url('images/ecranaccueil.jpg');
-            background-repeat: no-repeat; /* Assurez-vous que l'image ne se répète pas */
-            background-size: cover; /* Étirez l'image pour qu'elle couvre toute la page */
-            background-attachment: fixed; /* Fixez l'image en arrière-plan pour qu'elle reste en place lorsque vous faites défiler la page */
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
         }
     </style>
 </head>
 <body>
-    <?php include_once('header/navbar.php'); ?>
+    <?php require_once('header/navbar.php'); ?>
     <main>
         <?php
         if (isset($_GET['page'])) {
@@ -50,7 +50,7 @@ session_start();
                     break;
             }
         } else {
-            include_once 'pages/accueil';
+            include_once 'pages/accueil.php';
         }
         ?>
     </main>
